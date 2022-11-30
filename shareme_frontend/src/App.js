f851d2e98673a6cbbbad17da846bb31a9ef8ac23
daniel-akinyemi <danielakinyemi1999@gmail.com>
2022-11-30 21:47:24 +0100

    1st commit

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Routes, useNavigate} from 'react-router-dom'
+
+const App =()=>{
+    return(
+        <div>
+            hello World
+        </div>
+    )
+}
+
+export default App
